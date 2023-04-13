04490c60c196137d26f4a1f375fb89897726dc8b
Roman <roman.f.grusha@gmail.com>
2023-04-13 23:42:07 +0300

    getServerSession added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
-export default function DashboardPage() {
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  console.log(session, 'session');
+
   return (
     <>
       <h1>DashboardPage</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </>
   );
 }
