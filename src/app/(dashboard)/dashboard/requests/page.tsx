a4d95bd5970f858b6a292c31ffc26b91888e4113
Roman <roman.f.grusha@gmail.com>
2023-04-17 23:22:47 +0300

    FriendRequestsPage added

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,49 @@
+import { IUser, TFriendRequests } from '@/interfaces/global';
+import { authOptions } from '@/lib/auth';
+import { fetchRedis } from '@/lib/redis';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+
+export default async function FriendRequestsPage() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return notFound();
+  }
+
+  const senderIdList = await fetchRedis<TFriendRequests>(
+    'smembers',
+    `user:${session.user.id}:incoming_friend_requests`
+  );
+
+  const senderList = await Promise.all(
+    senderIdList.map(async (id) => {
+      try {
+        const senderRes = await fetchRedis<string>('get', `user:${id}`);
+        const senderParsed: IUser = JSON.parse(senderRes);
+        return {
+          id: senderParsed.id,
+          name: senderParsed.name,
+          email: senderParsed.email,
+          image: senderParsed.image,
+        };
+      } catch (error) {
+        console.log('Something went wrong while parsing sender', error);
+        return null;
+      }
+    })
+  );
+  // console.log(senderIdList, 'senderIdList');
+  console.log(senderList, 'senderList');
+  // console.log(typeof senderList, 'senderList');
+  return (
+    <main className='pt-8'>
+      <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+      <div className='flex flex-col gap-4'>
+        {/* <FriendRequests
+          incomingFriendRequests={incomingFriendRequests}
+          sessionId={session.user.id}
+        /> */}
+      </div>
+    </main>
+  );
+}
