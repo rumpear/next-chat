427aebc4e3ccedfa3facd5edbcbd19ac1635f706
Roman <roman.f.grusha@gmail.com>
2023-04-17 15:46:53 +0300

    LoginPage refactoring, Icons added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import Button from '@/components/ui/Button';
 import { signIn } from 'next-auth/react';
 import { toast } from 'react-hot-toast';
+import Button from '@/components/ui/Button';
+import Icons from '@/components/ui/Icons';
 
 const LoginPage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -20,25 +21,23 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='w-full flex flex-col items-center max-w-md space-y-8'>
-          <div className='flex flex-col items-center gap-8'>
-            Logo
-            <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-              Sign in to your account
-            </h2>
-            <Button
-              isLoading={isLoading}
-              onClick={handleGoogleLogin}
-              className='w-full max-w-sm mx-auto'
-            >
-              Google
-            </Button>
-          </div>
+    <main className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='w-full flex flex-col items-center max-w-md space-y-8'>
+        <div className='flex flex-col items-center gap-8'>
+          <Icons.Logo />
+          <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+            Sign in to your account
+          </h2>
+          <Button
+            isLoading={isLoading}
+            onClick={handleGoogleLogin}
+            className='w-full max-w-sm mx-auto'
+          >
+            <Icons.GoogleLogo /> Google
+          </Button>
         </div>
       </div>
-    </>
+    </main>
   );
 };
 
