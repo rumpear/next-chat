7b7b87b2687fd7714021121d323dffda9fe00d40
Roman <roman.f.grusha@gmail.com>
2023-04-18 23:13:15 +0300

    padding-top removed

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -2,7 +2,7 @@ import AddFriendForm from '@/components/AddFriendForm';
 
 export default async function DashboardAddPage() {
   return (
-    <main className='pt-8'>
+    <main>
       <h2 className='font-bold text-5xl mb-8'>Add a friend</h2>
       <AddFriendForm />
     </main>
