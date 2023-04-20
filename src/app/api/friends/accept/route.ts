70748db44cb860c025b86d7b9991670ae76d1b81
Roman <roman.f.grusha@gmail.com>
2023-04-20 23:17:01 +0300

    API for accepting friend added

---
+++ src/app/api/friends/accept/route.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { fetchRedis } from '@/lib/redis';
+
+export async function POST(req: Request) {
+    console.log('POST');
+    console.log(req, 'req');
+  try {
+    console.log('something happened');
+    const body = await req.json();
+    console.log(body, 'body');
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    const isFriend = await fetchRedis(
+      'sismember',
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+
+    if (isFriend) {
+      return new Response('Is already friends', { status: 400 });
+    }
+
+    const isFriendRequestExist = await fetchRedis(
+      'sismember',
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    );
+    console.log(isFriendRequestExist. 'isFriendRequestExist')
+    return new Response('Ok', { status: 200 });
+  } catch (error) {
+    console.log(error, 'error');
+  }
+}
