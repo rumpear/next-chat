c1396823fa2adca11dc77cce4f82f6ba347e430c
Roman <roman.f.grusha@gmail.com>
2023-05-03 23:34:43 +0300

    message & messageList schemas added

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -8,3 +8,14 @@ export const emailSchema = z.object({
     })
     .email({ message: 'Invalid email address' }),
 });
+
+export const messageSchema = z.object({
+  id: z.string(),
+  senderId: z.string(),
+  text: z.string(),
+  timeStamp: z.number(),
+});
+
+export const messageListSchema = z.array(messageSchema);
+
+export type Message = z.infer<typeof messageSchema>;
